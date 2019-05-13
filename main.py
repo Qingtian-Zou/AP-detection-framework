2fb7040edc8b72b0e01b9ce2b2ad02ba9507471d
Qingtian <qingtian.mill.zou@gmail.com>
2019-05-13 11:04:48 -0400

    Modularization of network log parsing.

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 import os
 import argparse
-from multiprocessing import Process
+from multiprocessing import Process, Pipe
 from AP_parsers import *
 from data_parsers import *
-
+from identifiers import *
 
 FLAGS=None
 
-def analyze_net_log(item):
-    p=Process()
-
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
+    parser.add_argument(
+        "--APs",
+        type=str,
+        default=".",
+        help="path to the attack pattern folder"
+    )
     parser.add_argument(
         "--net_logs",
         type=str,
@@ -31,18 +34,23 @@ if __name__=="__main__":
         default=".",
         help="path to the Windows event log folder"
     )
-    parser.add_argument(
-        "--APs",
-        type=str,
-        default=".",
-        help="path to the attack pattern folder"
-    )
     FLAGS,unparsed=parser.parse_known_args()
     data_parsing_jobs=[]
     identification_jobs=[]
 
     # TODO: for each folder above, the framework should:
     # 1) go through all the files in it and parse related files to parsers or other components.
+    tmp={}
     for item in os.listdir(FLAGS.net_logs):
-        analyze_net_log(item)
+        if item.split(".")[-1] not in ["pcapng", "pcap"]:
+            continue
+        parent_conn, child_conn = Pipe()
+        p=Process(target=network_log_parser.analyze_net_log,args=(os.path.join(FLAGS.net_logs,item),child_conn,))
+        p.start()
+        tmp[p]=parent_conn
+    pkts={}
+    for p in tmp:
+        rec=tmp[p].recv()
+        pkts[rec[0]]=rec[1]
+        p.join()
     # 2) create a watchdog monitor to monitor changes. New task should be scheduled if new log/AP files are put in, or files are updated.
